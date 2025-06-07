aeda1abb5d8fe5d96514d381deb9c2c112c91385
Jaideep0802 <jaideepbuds802@gmail.com>
2025-06-07 10:47:26 +0530

    minor_bug_fix

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
-
+import { useNavigate } from 'react-router-dom'
 const Result = () => {
 
     const { resultImage, image } = useContext(AppContext)
+
+    const navigate = useNavigate()
 
   return (
     <div className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
@@ -19,10 +21,10 @@ const Result = () => {
                 {/* right side */}
                 <div className='flex flex-col'>
                     <p className='font-semibold text-gray-600 mb-2'>Background Removed</p>
-                    <div className='rounded-md border border-gray-300 h-full relative bg-layer ocerflow-hidden'>
+                    <div className='rounded-md border border-gray-300 h-full relative bg-layer overflow-hidden'>
                         <img src= {resultImage ? resultImage : ""} alt="" />
                         {
-                            !resultImage && image && <div className='absolute right-1/2 bottom-1/2 tranform translate-x-1/2 translate-y-1/2'>
+                            !resultImage && image && <div className='absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2'>
                             <div className='border-4 border-violet-600 rounded-full h-12 w-12 border-t-transparent animate-spin'></div>
                         </div>
                         }
@@ -32,7 +34,7 @@ const Result = () => {
 
             {/* buttons */}
             {resultImage && <div className='flex justify-center sm:justify-end items-center flex-wrap gap-4 mt-6'>
-                    <button className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-700'>Try another Image</button>
+                    <button onClick={() => navigate('/')} className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-700 cursor-pointer'>Try another Image</button>
                     <a href={resultImage} download className='px-8 py-2.5 text-white text-sm bg-gradient-to-r from-violet-600 to-fuchsia-500 rounded-full hover:scale-105 transition-all duration-700'>Download Image</a>
                 </div>
             }   
